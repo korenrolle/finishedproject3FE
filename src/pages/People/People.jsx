10a8c7d0a2a6f7e92c7574c3d91509128a6846e4
Joshua Smith <joshua.smith@generalassemb.ly>
2022-12-21 08:42:06 -0700

    creating people form

--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -5,8 +5,18 @@ const People = (props) => {
     // define our state variable - []
     // react state
     const [people, setPeople] = useState([])
+    const [newForm, setNewForm] = useState({
+        name: "",
+        image: "",
+        title: "",
+    })
     // fetch endpoint
     const BASE_URL = "http://localhost:4000/people"
+
+    // create some local state for tracking people input (user) ++
+    // link this state to a controlled form (people) ++
+    // handlers (change ++ / submit )
+    // submit event will make a post request from our current comp.
 
     const getPeople = async () => {
         try {
@@ -14,7 +24,7 @@ const People = (props) => {
             // fetch grabs the data from API - (mongo)
             const allPeople = await response.json()
             // assuming no errors - translate to JS 
-            console.log(allPeople)
+            // console.log(allPeople)
             setPeople(allPeople)
             // store that data (from api) in react state
         } catch (err) {
@@ -22,8 +32,52 @@ const People = (props) => {
         }
     }
 
+    const handleChange = (e) => {
+        // console.log(newForm)
+        const userInput = {...newForm}
+        userInput[e.target.name] = e.target.value
+        setNewForm(userInput)
+    }
+
+    const handleSubmit = async (e) => {
+        // 0. prevent default (event object method)
+        e.preventDefault()
+        // 1. capturing our local state
+        const currentState = {...newForm}
+        // check any fields for property data types / truthy value (function call - stretch)
+        try{
+            const requestOptions = {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(currentState)
+            } 
+            // 2. specify request method , headers, Content-Type
+            // 3. make fetch to BE - sending data (requestOptions)
+
+            // 3a fetch sends the data to API - (mongo)
+            const response = await fetch(BASE_URL, requestOptions)
+            // 4. check our response - 
+            // 5. parse the data from the response into JS (from JSON) 
+            const createdPerson = await response.json()
+            console.log(createdPerson)
+            // update local state with response (json from be)
+            setPeople([...people, createdPerson])
+            // reset newForm state so that our form empties out
+            setNewForm({
+                name: "",
+                image: "",
+                title: "",
+            })
+
+        }catch(err) {
+            console.log(err)
+        }
+    }
     const loaded = () => {
-        return (
+        return (<>
+            
             <section className="people-list">
                 {people?.map((person) => {
                     return (
@@ -36,7 +90,9 @@ const People = (props) => {
                     )
                 })
                 }
-            </section>)
+            </section>
+        </>
+        )
     }
 
     const loading = () => (
@@ -61,8 +117,54 @@ const People = (props) => {
     // react mount -> 
     return (
         <div>
+            <section>
+                <h2>Create a new person</h2>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                    <label htmlFor='name'>
+                        Name
+                        <input 
+                            type="text" 
+                            id="name"
+                            name="name" 
+                            placeholder="enter a person's name" 
+                            value={newForm.name}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    </div>
+                   <div>
+                    <label htmlFor='image'>
+                        Image
+                        <input 
+                            type="text" 
+                            id="image"
+                            name="image" 
+                            placeholder="enter a person's image" 
+                            value={newForm.image}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    </div>
+                    <div>
+                    <label htmlFor='title'>
+                        Title
+                        <input 
+                            type="text" 
+                            id="title"
+                            name="title" 
+                            placeholder="enter a person's title" 
+                            value={newForm.title}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <br/>
+                    <input type="submit" value="Create a new person" />
+                    </div>
+                </form>
+            </section>
             {people && people.length ? loaded() : loading()}
-    </div >
+        </div >
     )
 
 }
